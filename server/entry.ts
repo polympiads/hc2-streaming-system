6a5832140e567756c36cf1fae12147ad542eedd6
theo.hollender <theo.hollender@gmail.com>
2025-04-09 18:14:31 +0200

    dev: added base server

--- server/entry.ts
+++ server/entry.ts
@@ -1,2 +1,31 @@
 
-console.log("Hello, World !")
+const express  = require('express');
+const http     = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+
+const server = http.createServer(app);
+const io     = socketIO(server);
+
+app.use(express.static('public'))
+
+io.on('connection', socket => {
+    console.log('New client connected:', socket.id);
+
+    socket.on('offer', data => {
+        socket.broadcast.emit('offer', data); // send offer to others
+    });
+
+    socket.on('answer', data => {
+        socket.broadcast.emit('answer', data); // send answer back
+    });
+
+    socket.on('ice-candidate', data => {
+        socket.broadcast.emit('ice-candidate', data); // forward ICE candidates
+    });
+});
+  
+server.listen(3000, () => {
+    console.log('Signaling server running on http://localhost:3000');
+});

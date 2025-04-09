849950f94840d3f47659493322dbf8462b9a427d
theo.hollender <theo.hollender@gmail.com>
2025-04-09 21:20:34 +0200

    dev : added authentication protocol

--- public/source/auth.js
+++ public/source/auth.js
@@ -1,6 +1,68 @@
 
 class AuthManager {
-    constructor () {
-        
+    getSecret () {
+        return getCookie("x-secret");
+    }
+    getSession () {
+        return getCookie("x-session");
+    }
+    clearSession () {
+        clearCookie("x-secret");
+        clearCookie("x-session");
+    }
+
+    authenticate (username, password) {
+        this.socket.emit("authenticate", { "username": username, "password": password })
+    }
+
+    onAuthenticate (payload) {
+        if (payload.session === null
+         || payload.secret  === null
+        ) {
+            this.clearSession();
+            throw "Authentication failed";
+        }
+
+        setCookie("x-secret",  payload.secret);
+        setCookie("x-session", payload.session);
+    }
+    onSession (payload) {
+        if (payload.success) return ;
+
+        this.clearSession();
+        throw "Authentication failed";
+    }
+    challenge (challenge) {
+        const session = this.getSession();
+        const secret  = this.getSecret();
+        if (session === null || session === undefined) throw "Missing session.";
+        if (secret  === null || secret  === undefined) throw "Missing secret.";
+
+        const crypto = new SubtleCrypto();
+        const target = crypto.digest("SHA256", session + secret + challenge.suffix);
+
+        this.socket.emit("onChallenge", { "hash": target });
+    }
+    tryBind () {
+        const session = this.getSession();
+        const secret  = this.getSecret();
+        if (session === null || session === undefined) return ;
+        if (secret  === null || secret  === undefined) return ;
+
+        this.socket.emit("bindSession", { "session": session });
+    }
+
+    bindSocket (socket) {
+        this.socket = socket;
+        this.tryBind();
+
+        socket.on("onAuthenticate", payload => this.onAuthenticate(payload))
+        socket.on("onSession", session => this.onSession(session))
+        socket.on("challenge", request => this.challenge(request))
     }
 }
+
+const AUTH_MANAGER = new AuthManager();
+
+const authBindSocket = (socket) => AUTH_MANAGER.bindSocket(socket);
+const authenticate   = (username, password) => AUTH_MANAGER.authenticate(username, password);
--- public/source/cookie.js
+++ public/source/cookie.js
@@ -1,12 +1,40 @@
 
 class CookieManager {
-    constructor () {}
+    constructor () {
+        this.innerProperty = null;
+    }
 
+    load () {
+        if (this.innerProperty !== null) return ;
+        
+        this.innerProperty = {};
+        const array = document.cookie.split(";");
+        for (let el of array) {
+            el = el.trim();
+            if (el == "") continue ;
+
+            let [key, value] = el.split("=", 2);
+            value = JSON.parse(atob(value.replaceAll("%", "=")))
+            this.innerProperty[key] = value;
+        }
+    }
+
+    getCookie (key) {
+        this.load();
+        return this.innerProperty[key];
+    }
     setCookie (key, value) {
-
+        this.load();
+        this.innerProperty[key] = value;
+        document.cookie = `${key}=${btoa(JSON.stringify(value)).replaceAll("=", "%")};`
     }
 };
 
 const COOKIE_MANAGER = new CookieManager();
 
-const setCookie = COOKIE_MANAGER.setCookie
+const setCookie = (key, value) => COOKIE_MANAGER.setCookie(key, value);
+const getCookie = (key)        => COOKIE_MANAGER.getCookie(key);
+
+const clearCookie = (key) => {
+    browser.cookies.remove(key);
+}

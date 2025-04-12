f37952e5b340302bd8fcf268194d84ca803e37f2
theo.hollender <theo.hollender@gmail.com>
2025-04-12 08:17:55 +0200

    added stream buffers

--- public/source/admin.js
+++ public/source/admin.js
@@ -4,6 +4,8 @@ function create_feed_div (channel) {
     div.innerHTML = `<div>
         <div>Channel : <span id="ucont"></span></div>
         <video id="tvideo" width="320" height="180" autoplay muted>
+        <button id="buffer0">EmitBuffer0</button>
+        <button id="buffer1">EmitBuffer1</button>
     </div>`
 
     const ucont = div.querySelector("#ucont")
@@ -12,10 +14,13 @@ function create_feed_div (channel) {
     const video = div.querySelector("#tvideo")
     video.id = `tvideo-${channel}`;
 
+    const bf0 = div.querySelector("buffer0");
+    const bf1 = div.querySelector("buffer1");
+
     const cont = div.children.item(0);
     div.removeChild(cont)
 
-    return [ cont, video.id ];
+    return [ cont, video.id, bf0, bf1 ];
 }
 
 class AdminManager {
@@ -26,11 +31,14 @@ class AdminManager {
     createChannel (channel) {
         this.channels.add(channel);
 
-        const [cont, video] = create_feed_div(channel);
+        const [cont, video, bf0, bf1] = create_feed_div(channel);
         document.querySelector("#admin-list").appendChild(cont);
 
         const viewer = new RTCViewer(video, channel + "_low");
         this.channel_properties[channel] = [ viewer ];
+
+        bf0.onclick = () => SOCKET.emit("enableCamera", { channel: channel + "_high", buffer: 1 })
+        bf1.onclick = () => SOCKET.emit("enableCamera", { channel: channel + "_high", buffer: 2 })
     }
     exposeChannel (channel) {
         if (!this.channels.has(channel))

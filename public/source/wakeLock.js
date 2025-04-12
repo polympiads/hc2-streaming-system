82a33128bfc2d8785bfa8c0d2cd578f75de3292d
Eliott Tornassat <eliott.tornassat@epfl.ch>
2025-04-12 15:55:37 +0200

    prevent device from waking up

---
+++ public/source/wakeLock.js
@@ -0,0 +1,15 @@
+const canWakeLock = () => 'wakeLock' in navigator;
+
+let wakelock;
+async function lockWakeState() {
+  if(!canWakeLock()) return;
+  try {
+    wakelock = await navigator.wakeLock.request();
+    wakelock.addEventListener('release', () => {
+      console.log('Screen Wake State Locked:', !wakelock.released);
+    });
+    console.log('Screen Wake State Locked:', !wakelock.released);
+  } catch(e) {
+    console.error('Failed to lock wake state with reason:', e.message);
+  }
+}

f22e283c4b7d3e920cac0ab2c3d921a864fbfbc3
theo.hollender <theo.hollender@gmail.com>
2025-04-12 08:58:20 +0200

    added buffer swapping

--- public/source/admin.js
+++ public/source/admin.js
@@ -53,6 +53,6 @@ const ADMIN_MANAGER = new AdminManager();
 function adminBindSocket (socket) {
     socket.on("exposeChannel", channel => ADMIN_MANAGER.exposeChannel(channel.camera))
 
-    document.querySelector("#swap_buffers_0", () => socket.emit("swapBuffers", { front: 0 }))
-    document.querySelector("#swap_buffers_1", () => socket.emit("swapBuffers", { front: 1 }))
+    document.querySelector("#swap_buffers_0").onclick = () => socket.emit("swapBuffers", { front: 0 })
+    document.querySelector("#swap_buffers_1").onclick = () => socket.emit("swapBuffers", { front: 1 })
 }
